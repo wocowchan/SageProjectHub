8663654ff684f775026425cfdf07821ec038c762
jackylaucf <jackycflau@gmail.com>
2018-03-29 15:17:58 +0800

    initial

---
+++ app/src/main/java/hk/com/sagetech/sageprojecthub/MainActivity.java
@@ -0,0 +1,50 @@
+package hk.com.sagetech.sageprojecthub;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView mTextMessage;
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_home:
+                    mTextMessage.setText(R.string.title_home);
+                    return true;
+                case R.id.navigation_projects:
+                    mTextMessage.setText(R.string.title_projects);
+                    return true;
+                case R.id.navigation_board:
+                    mTextMessage.setText(R.string.title_board);
+                    return true;
+                case R.id.navigation_notification:
+                    mTextMessage.setText(R.string.title_notification);
+                    return true;
+                case R.id.navigation_menu:
+                    mTextMessage.setText(R.string.title_menu);
+                    return true;
+            }
+            return false;
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mTextMessage = (TextView) findViewById(R.id.message);
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        BottomNavigationViewHelper.removeShiftMode(navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+    }
+}
